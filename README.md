5bf69c66b76a5c3cedff67274dc52d1cf81c9734
nyx <akasarujiji@gmail.com>
2025-11-01 23:14:15 +0100

    Tidying up lab_02 solution.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Java labs
+Repository with solutions to JPWP labs exercises.
+```
+/lab_java
+ \lab_01
+  |solution
+  |...
+ \lab_02
+  |solution
+  |...
+...
+```
--- lab_02/src/main/java/org/example/Main.java
+++ lab_02/src/main/java/org/example/Main.java
@@ -1,184 +1,263 @@
 package org.example;
+import java.security.spec.ECField;
+import java.util.Arrays;
 
 public class Main {
-  public int counter = 0;
+  /*
+   * Main function
+   */
   public void main() {
     Greet();
     input();
-    Suma();
-    Kalkulator();
-    Parzyste();
-    Pierwsza();
+    Sum();
+    Calculator();
+    Even();
+    isAPrime();
   }
+
+  /*
+   * Greeting function:
+   * Just greets when called.
+   */
   public void Greet(){
-    counter++;
-    IO.println(String.format("Hello world!"));
-    IO.println(counter);
+    if (expand_ex_sol("Zadanie2_1. Hello World")){
+      IO.println("  Hello World!!!");
+    }
   }
+
+  /*
+   * User input function:
+   * Asks for an integer input and echoes it. 
+   */
   public void input(){
-    counter = Integer.parseInt(IO.readln("Input a number: "));
-    IO.println("Your number: "+counter);
-  }
-  public void Suma(){
-    IO.println("Enter a and b and program will output sum:");
-    double a = Double.parseDouble(IO.readln("Enter a="));
-    double b = Double.parseDouble(IO.readln("Enter b="));
-    IO.println("Sum: " + (a+b));
-  }
-  public void Kalkulator() {
-    while (true) {
-      int op = 9;
-      IO.println("Calculator choose an option:");
-      IO.println(" [0]: Exit calc.");
-      IO.println(" [1]: Suma");
-      IO.println(" [2]: Różnica");
-      IO.println(" [3]: Iloczyn");
-      IO.println(" [4]: Iloraz");
-      IO.println(" [5]: Pole prostokąta");
-      IO.println(" [6]: Pole trójkąta");
-      IO.println(" [7]: c^2=[?]=a^2+b^2");
-      IO.println(" [8]: Najmniejsza liczba z podanych a,b i c");
-      try {
-        op = Integer.parseInt(IO.readln("Option: "));
-      }catch(Exception e){
-        IO.println("ERROR: "+e);
-        continue;
+    if (expand_ex_sol("Zadanie2_2. Wypisywanie liczby")){
+      while (true){
+        try{
+          int num = Integer.parseInt(IO.readln("Input a integer: "));
+          IO.println("  Your integer: "+ num);
+          break;
+        }catch (Exception e){
+          IO.println("Exception catched: "+e);
+        }
       }
-      switch (op) {
-      case 0:
-        IO.println("Closing calc...");
+    }
+  }
+
+  /*
+   * Output sum of a and b numbers
+   */
+  public void Sum(){
+    if (expand_ex_sol("Zadanie2_3. Suma")){
+      IO.println("Enter a and b and program will output sum:");
+      while (true){
+        try{
+          double a = Double.parseDouble(IO.readln(" a = "));
+          double b = Double.parseDouble(IO.readln(" b = "));
+          IO.println("  a + b: " + (a+b));
+          break;
+        }catch(Exception e){
+          IO.println("Exception catched: " + e);
+        }
+      }
+    }
+  }
+
+  /*
+   * Simple calculator
+   */
+  public void Calculator() {
+    if (expand_ex_sol("Zadanie2_4. Kalkulator")){
+      while (true) {
+        int op = 9;
+        IO.println("Calculator: choose an option:");
+        IO.println(" [0]: Exit calc.");
+        IO.println(" [1]: Sum");
+        IO.println(" [2]: Subtraction");
+        IO.println(" [3]: Product");
+        IO.println(" [4]: Quotient");
+        IO.println(" [5]: Rectangle area");
+        IO.println(" [6]: Triangle area");
+        IO.println(" [7]: Sum of squares = a^2 + b^2 ");
+        IO.println(" [8]: Smallest number");
+        try {
+          op = Integer.parseInt(IO.readln("Option: "));
+        }catch(Exception e){
+          IO.println("Exception catched: "+e);
+          continue;
+        }
+        switch (op) {
+          case 0:
+            IO.println("Closing calc...");
+            return;
+          case 1:
+            IO.println("Sum:");
+            try {
+              double a = Double.parseDouble(IO.readln(" a = "));
+              double b = Double.parseDouble(IO.readln(" b = "));
+              IO.println("  a + b = " + (a + b));
+            }catch (Exception e){
+              IO.println("Exception catched: "+e);
+              break;
+            }
+            break;
+          case 2:
+            try {
+              IO.println("Subtraction:");
+              double a = Double.parseDouble(IO.readln(" a = "));
+              double b = Double.parseDouble(IO.readln(" b = "));
+              IO.println("  a - b = "+(a-b));
+            }catch (Exception e){
+              IO.println("Exception catched: "+e);
+              break;
+            }
+            break;
+          case 3:
+            try {
+              IO.println("Product:");
+              double a = Double.parseDouble(IO.readln(" a = "));
+              double b = Double.parseDouble(IO.readln(" b = "));
+              IO.println("  a * b = "+(a*b));
+            }catch (Exception e){
+              IO.println("Exception catched: "+e);
+              break;
+            }
+            break;
+          case 4:
+            try {
+              IO.println("Quotient:");
+              double a = Double.parseDouble(IO.readln(" a = "));
+              double b = Double.parseDouble(IO.readln(" b = "));
+              if (b==0) throw new Exception("Division by zero");
+              else IO.println(" a / b = "+(a/b));
+            }catch (Exception e){
+              IO.println("Exception catched: "+e);
+              break;
+            }
+            break;
+          case 5:
+            try {
+              IO.println("Rectangle area:");
+              double a = Double.parseDouble(IO.readln(" side a = "));
+              double b = Double.parseDouble(IO.readln(" side b = "));
+              if ( a <= 0 || b <= 0 ) throw new Exception("Side length has to be greater than zero.");
+              IO.println("  Area = "+(a*b));
+            }catch (Exception e){
+              IO.println("Exception catched: "+e);
+              break;
+            }
+            break;
+          case 6:
+            try {
+              IO.println("Triangle area:");
+              double a = Double.parseDouble(IO.readln(" side a ="));
+              double b = Double.parseDouble(IO.readln(" side b ="));
+              double c = Double.parseDouble(IO.readln(" side c ="));
+              if ( a <= 0 || b <= 0 || c <= 0 ) throw new Exception("Side length has to be greater than zero.");
+              double p = ( a + b + c ) / 2;
+              IO.println("  Area = " + ( Math.sqrt( p * ( p - a ) * ( p - b ) * ( p - c ) ) ) );
+            }catch (Exception e){
+              IO.println("Exception catched: "+e);
+              break;
+            }
+            break;
+          case 7:
+            try {
+              IO.println("Sum of squares:");
+              double a = Double.parseDouble(IO.readln(" a = "));
+              double b = Double.parseDouble(IO.readln(" b = "));
+              IO.println("  a^2 + b^2 = " + ( ( Math.pow(a, 2) ) + ( Math.pow(b, 2) ) ) );
+            }catch (Exception e){
+              IO.println("Exception catched: "+e);
+              break;
+            }
+            break;
+          case 8:
+            try {
+              IO.println("Smallest number:");
+              double a = Double.parseDouble(IO.readln(" a = "));
+              double b = Double.parseDouble(IO.readln(" b = "));
+              double c = Double.parseDouble(IO.readln(" c = "));
+              double d = a;
+              if (b<d) d=b;
+              if (c<d) d=c;
+              IO.println("  Smallest number = "+d);
+            }catch (Exception e){
+              IO.println("Exception catched: "+e);
+              break;
+            }
+            break;
+          default:
+            IO.println("Wrong option, choose one from [0,1,2,3,4,5,6,7,8]");
+            break;
+        }
+      }
+    }
+
+  }
+
+  /*
+   * Output a sequence of even numbers in 0 to 100 range.
+   */
+  public void Even(){
+    if (expand_ex_sol("Zadanie2_5. Liczby parzyste")){
+      IO.println("Even numbers inside 0 to 100 range:");
+      IO.print("  [");
+      for (int i=0;i<=100;i=i+2){
+        IO.print(i+", ");
+      }
+      IO.println("]");
+    }
+  }
+
+  /*
+   * Check whether input number is a prime
+   */
+  public void isAPrime(){
+    if (expand_ex_sol("Zadanie2_6. Test liczby pierwszej.")){
+      IO.println("Check whether a number is a prime:");
+      int n = 0;
+      while(true) {
+        try {
+          n = Integer.parseInt(IO.readln("Input a integer: "));
+          break;
+        } catch (Exception e) {
+          IO.println("Exception catched: "+e);
+        }
+      }
+      if (n <= 1){ // Prime number is greater than 1
+        IO.println("  Lesser than or equal to 1 is not a prime number!");
         return;
-      case 1:
-        IO.println("Suma:");
-        try {
-          double a = Double.parseDouble(IO.readln(" a ="));
-          double b = Double.parseDouble(IO.readln(" b ="));
-          IO.println("Suma a+b=" + (a + b));
-        }catch (Exception e){
-          IO.print("ERROR: "+e);
-          break;
-        }
-        break;
-      case 2:
-        try {
-        IO.println("Różnica:");
-        double a = Double.parseDouble(IO.readln(" a ="));
-        double b = Double.parseDouble(IO.readln(" b ="));
-        IO.println("Suma a-b="+(a-b));
-        }catch (Exception e){
-          IO.print("ERROR: "+e);
-          break;
-        }
-        break;
-      case 3:
-        try {
-        IO.println("Iloczyn:");
-          double a = Double.parseDouble(IO.readln(" a ="));
-          double b = Double.parseDouble(IO.readln(" b ="));
-        IO.println("Suma a*b="+(a*b));
-        }catch (Exception e){
-          IO.print("ERROR: "+e);
-          break;
-        }
-        break;
-      case 4:
-        try {
-        IO.println("Iloraz:");
-          double a = Double.parseDouble(IO.readln(" a ="));
-          double b = Double.parseDouble(IO.readln(" b ="));
-        if (b==0) IO.println("Nie dzieli się przez zero!!");
-        else IO.println("Suma a/b="+(a/b));
-        }catch (Exception e){
-          IO.print("ERROR: "+e);
-          break;
-        }
-        break;
-      case 5:
-        try {
-        IO.println("Pole prostokąta:");
-          double a = Double.parseDouble(IO.readln(" bok a ="));
-          double b = Double.parseDouble(IO.readln(" bok b ="));
-        IO.println("Pole="+(a*b)+"[j^2]");
-        }catch (Exception e){
-          IO.print("ERROR: "+e);
-          break;
-        }
-        break;
-      case 6:
-        try {
-        IO.println("Pole trójkąta:");
-          double a = Double.parseDouble(IO.readln(" bok a ="));
-          double b = Double.parseDouble(IO.readln(" bok b ="));
-        double c = Double.parseDouble(IO.readln(" bok c ="));
-        if (a<=0 || b<=0 || c<=0){
-          IO.println("Side of a triangle can't be zero or lesser than zero.");
-          break;
-        }
-        double p = (a+b+c)/2;
-        IO.println("Pole="+(Math.sqrt(p*(p-a)*(p-b)*(p-c)))+"[j^2]");
-        }catch (Exception e){
-          IO.print("ERROR: "+e);
-          break;
-        }
-        break;
-      case 7:
-        try {
-        IO.println("a^2+b^2=c^2:");
-          double a = Double.parseDouble(IO.readln(" a ="));
-          double b = Double.parseDouble(IO.readln(" b ="));
-        IO.println("a^2+b^2="+((Math.pow(a,2))+(Math.pow(b,2))));
-        }catch (Exception e){
-          IO.print("ERROR: "+e);
-          break;
-        }
-        break;
-      case 8:
-        try {
-        IO.println("Najmniejsza liczba:");
-          double a = Double.parseDouble(IO.readln(" a ="));
-          double b = Double.parseDouble(IO.readln(" b ="));
-          double c = Double.parseDouble(IO.readln(" c ="));
-        double d = a;
-        if (b>a) d=b;
-        if (c>a) d=c;
-        IO.println("Najmniejsza liczba = ["+d+"]");
-        }catch (Exception e){
-          IO.print("ERROR: "+e);
-          break;
-        }
-        break;
-      default:
-        IO.println("Wrong option, choose one from [0,1,2,3,4,5,6,7,8]");
-        break;
       }
+        // trial division method but with non prime denominators included so the program is not optimized
+      for (int i = 2; i < n; i++){
+        if (n % i == 0){ 
+          IO.println("  It is not a prime number.");
+          return;
+        }
+      }
+      IO.println("  It is a prime number!");
     }
   }
-  public void Parzyste(){
-    IO.println("Liczby parzyste od zera do 100:");
-    IO.print("[");
-    for (int i=0;i<=100;i=i+2){
-      IO.print(i+", ");
-    }
-    IO.print("]");
-  }
-  public void Pierwsza(){
-    IO.println("Check whether a number is a prime:");
-    double n = 0;
-    while(true) {
-      try {
-        n = Integer.parseInt("Input a number: ");
+
+  /*
+   * Ask if to expand solution.
+   */
+  public boolean expand_ex_sol(String ex){
+    while(true){
+      try{
+        String c = IO.readln("Do you want to execute a solution to ["+ex+"] exercise: [Y|y] [N|n]? => ").toLowerCase();
+        if (c.equals("n")) {
+          IO.println("Skipping solution to ["+ex+"] exercise.");
+          return false;
+        }else if ( !(c.equals("y")) ){
+          throw new Exception("Input error: Wrong input, expected [N|n|Y|y] statement, got:["+c+"]");
+        }
+        IO.println("Executing solution:");
         break;
-      } catch (Exception e) {
-        IO.println("ERROR: "+e);
+      }catch(Exception e){
+        IO.println("Catched exception: " + e);
       }
     }
-    if (n <= 1)
-      IO.println("Not a prime number");
-    // Check divisibility from 2 to n-1
-    for (int i = 2; i < n; i++)
-    {
-      if (n % i == 0) IO.println("Not a prime number");
-    }
-    IO.println("It is a prime number!");
+    return true;
   }
 }
